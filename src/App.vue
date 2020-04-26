17d5ff4a841e81aee512fecf816012f77ef83952
DjMVdO <43639815+DjMVdO@users.noreply.github.com>
2020-04-26 15:48:18 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>  
+    <h1>{{ title + i+ times}}</h1>
+    <button @click="iAmClicked()">I dare you to click!</button>
+  </div>
+</template>
+ 
+<script>
+    export default {
+       
+        data() {
+            return {
+                title: 'Well done ! You have cliked : ',
+                i : 0 ,
+                times :" times",
+            }
+        },
+        methods: {
+
+        
+            iAmClicked() {
+                this.i +=1;
+                this.title = "Good job, you have cliked : ";
+                this.times =" times";
+            },
+
+           
+        }
+    };
+</script>
